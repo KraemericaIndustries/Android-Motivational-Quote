ee4b26a0c1d39c9ec1b25b40a2fa623ff6880d8c
Bob Kraemer <bob.kraemer.git@gmail.com>
2023-04-07 15:02:56 -0400

    Initial commit

---
+++ app/src/main/java/com/kraemericaindustries/aadb2motivationalquotechallenge/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kraemericaindustries.aadb2motivationalquotechallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
